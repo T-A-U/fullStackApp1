bace54b62f7e0a72cbb0af40100f396bc77bb2c4
T-A-U <153134801+T-A-U@users.noreply.github.com>
2025-11-03 10:50:39 -0500

    Internal functionality, now to display on DOM

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://T-A-U:qeoft4RRJB5oIzsj@cluster0.htcpnep.mongodb.net/ZeldaGames?appName=Cluster0', 
+    'dbName': 'ZeldaGames'
+};
